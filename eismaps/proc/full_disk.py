3d0be32de277046a40f2e53f44fc8006fa4c633e
jamesmckevitt <james.mckevitt@outlook.com>
2024-08-05 16:32:20 +0200

    ability to not apply rotation

--- eismaps/proc/full_disk.py
+++ eismaps/proc/full_disk.py
@@ -24,7 +24,7 @@ def safe_load_map(map_file):
         map = None
     return map
 
-def make_helioprojective_map(map_files, save_dir, wavelength, measurement, overlap, preserve_limb=None, save_fit=False, save_plot=False, plot_ext='png', plot_dpi=300):
+def make_helioprojective_map(map_files, save_dir, wavelength, measurement, overlap, apply_rotation=True, preserve_limb=None, save_fit=False, save_plot=False, plot_ext='png', plot_dpi=300):
     """
     Make a helioprojective full disk map from a list of maps.
     """
@@ -76,51 +76,57 @@ def make_helioprojective_map(map_files, save_dir, wavelength, measurement, overl
 
         map = sunpy.map.Map(map_file)
 
-        if preserve_limb=='drag':  # Manually drag rasters (and don't distort) with a custom point
+        if apply_rotation:
 
-            def differental_rotate_map_by_drag(map, point):
-                # Calculate the time difference between the map and the first map
-                map_time = datetime.strptime(map.meta['date_obs'], '%Y-%m-%dT%H:%M:%S.%f')
-                first_map_time = datetime.strptime(first_map.meta['date_obs'], '%Y-%m-%dT%H:%M:%S.%f')
-                duration = map_time - first_map_time
-                # Convert the duration to an astropy time object
-                duration = u.Quantity(duration.total_seconds(), u.s)
-                # Rotate the point by the differential rotation
-                diffrot_point = SkyCoord(RotatedSunFrame(base=point, duration=duration))
-                transformed_diffrot_point = diffrot_point.transform_to(map.coordinate_frame)
-                # Calculate the difference between the original and transformed points
-                shift_x = transformed_diffrot_point.Tx - point.Tx
-                shift_y = transformed_diffrot_point.Ty - point.Ty
-                # Shift the map by the difference
-                map = map.shift_reference_coord(shift_x, shift_y)
-                return map
+            if preserve_limb=='drag':  # Manually drag rasters (and don't distort) with a custom point
 
-            if sunpy.map.is_all_off_disk(map):  # This map is completely off disk, and so can't be shifted
-                pass
-            elif sunpy.map.contains_limb(map):  # Some of the map is off limb, so need to carefully choose the pixels to drag
-                pixel_coords = sunpy.map.all_coordinates_from_map(map)
-                distances = np.sqrt(pixel_coords.Tx.value ** 2 + pixel_coords.Ty.value ** 2)
-                closest_flat_index = np.argmin(distances)
-                closest_2d_index = np.unravel_index(closest_flat_index, distances.shape)
-                closest_coords = pixel_coords[closest_2d_index]
-                map = differental_rotate_map_by_drag(map, closest_coords)
-            else: # This map is completely on disk, so can be shifted as normal
-                map = differental_rotate_map_by_drag(map, map.center)
+                def differental_rotate_map_by_drag(map, point):
+                    # Calculate the time difference between the map and the first map
+                    map_time = datetime.strptime(map.meta['date_obs'], '%Y-%m-%dT%H:%M:%S.%f')
+                    first_map_time = datetime.strptime(first_map.meta['date_obs'], '%Y-%m-%dT%H:%M:%S.%f')
+                    duration = map_time - first_map_time
+                    # Convert the duration to an astropy time object
+                    duration = u.Quantity(duration.total_seconds(), u.s)
+                    # Rotate the point by the differential rotation
+                    diffrot_point = SkyCoord(RotatedSunFrame(base=point, duration=duration))
+                    transformed_diffrot_point = diffrot_point.transform_to(map.coordinate_frame)
+                    # Calculate the difference between the original and transformed points
+                    shift_x = transformed_diffrot_point.Tx - point.Tx
+                    shift_y = transformed_diffrot_point.Ty - point.Ty
+                    # Shift the map by the difference
+                    map = map.shift_reference_coord(shift_x, shift_y)
+                    return map
 
-            ### TODO: add map data to array of same size as full disk data array, for combination below
+                if sunpy.map.is_all_off_disk(map):  # This map is completely off disk, and so can't be shifted
+                    pass
+                elif sunpy.map.contains_limb(map):  # Some of the map is off limb, so need to carefully choose the pixels to drag
+                    pixel_coords = sunpy.map.all_coordinates_from_map(map)
+                    distances = np.sqrt(pixel_coords.Tx.value ** 2 + pixel_coords.Ty.value ** 2)
+                    closest_flat_index = np.argmin(distances)
+                    closest_2d_index = np.unravel_index(closest_flat_index, distances.shape)
+                    closest_coords = pixel_coords[closest_2d_index]
+                    map = differental_rotate_map_by_drag(map, closest_coords)
+                else: # This map is completely on disk, so can be shifted as normal
+                    map = differental_rotate_map_by_drag(map, map.center)
 
-        elif preserve_limb=='spherical_screen':
+                ### TODO: add map data to array of same size as full disk data array, for combination below
 
-            # Apply differential rotation to the map, compared to the time of the fd_map
-            with propagate_with_solar_surface(rotation_model='howard'):
-                with SphericalScreen(map.observer_coordinate, only_off_disk=True):
+            elif preserve_limb=='spherical_screen':
+
+                # Apply differential rotation to the map, compared to the time of the fd_map
+                with propagate_with_solar_surface(rotation_model='howard'):
+                    with SphericalScreen(map.observer_coordinate, only_off_disk=True):
+                        map = map.reproject_to(fd_map.wcs, algorithm='exact')
+
+            else:
+
+                # Apply differential rotation to the map, compared to the time of the fd_map
+                with propagate_with_solar_surface():
                     map = map.reproject_to(fd_map.wcs, algorithm='exact')
 
         else:
-
-            # Apply differential rotation to the map, compared to the time of the fd_map
-            with propagate_with_solar_surface():
-                map = map.reproject_to(fd_map.wcs, algorithm='exact')
+            
+            map = map.reproject_to(fd_map.wcs, algorithm='exact')
 
         if overlap == 'max':
             combined_data = np.where(np.isnan(combined_data), map.data, np.nanmax([combined_data, map.data], axis=0))
