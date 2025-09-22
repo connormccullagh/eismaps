6ebd61d982a2b805e570601e705798c49fb93c07
connormccullagh <connor.mccullagh321@gmail.com>
2025-09-23 00:26:59 +0100

    Update find.py to allow time range specified

--- eismaps/utils/find.py
+++ eismaps/utils/find.py
@@ -32,3 +32,25 @@ def main(ext, dir, unique_times=False):
 
     collected_files.sort()
     return collected_files
+    
+def find_files_in_range(ext, folder, start_str, end_str):
+    from datetime import datetime
+    import datetime as dt
+    import os
+    
+    start_dt = datetime.strptime(start_str, "%Y%m%d_%H%M%S")
+    end_dt   = datetime.strptime(end_str, "%Y%m%d_%H%M%S")
+    files_in_range = []
+
+    for fname in os.listdir(folder):
+        if fname.startswith('eis_') and fname.endswith(ext):
+            ts_str = fname[4:].split('.')[0]
+            try:
+                file_dt = datetime.strptime(ts_str, "%Y%m%d_%H%M%S")
+            except ValueError:
+                continue
+            if start_dt <= file_dt <= end_dt:
+                files_in_range.append(os.path.join(folder, fname))
+
+    files_in_range.sort()
+    return files_in_range
